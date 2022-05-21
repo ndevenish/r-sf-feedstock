9af1511ca06355ecbc0dfe63cad48be5f64e5036
Nicholas Devenish <ndevenish@gmail.com>
2022-05-21 12:49:14 +0100

    Test for both mac platforms in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,17 +6,17 @@ export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
 # NB: unsure if this is even used...
 export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 # This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
-if [[ ${target_platform} == osx-64 ]]; then
+if [[ ${target_platform} == osx-64 ]] || [[ ${target_platform} == osx-arm64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
 fi
 
-if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
+if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]] || [[ ${target_platform} == osx-arm64 ]]; then
   export DISABLE_AUTOBREW=1
   ${R} CMD INSTALL --build .
 else
   mkdir -p "${PREFIX}"/lib/R/library/sf
   mv ./* "${PREFIX}"/lib/R/library/sf
-  if [[ ${target_platform} == osx-64 ]]; then
+  if [[ ${target_platform} == osx-64 ]] || [[ ${target_platform} == osx-arm64 ]]; then
     pushd "${PREFIX}"
       for libdir in lib/R/lib lib/R/modules lib/R/library lib/R/bin/exec sysroot/usr/lib; do
         pushd "${libdir}" || exit 1
